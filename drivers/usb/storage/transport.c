d3e6833c72da435f9ca4180a12f4d2660de4093a
Josue Rivera <prbassplayer@gmail.com>
2014-04-07 15:34:02 +0200

    Fix uninitialized variable in drivers/usb/storage/transport  fix variables partial, result
    Change-Id: I8d08221114ed09c35798d3888532d34e3b4791fb
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- drivers/usb/storage/transport.c
+++ drivers/usb/storage/transport.c
@@ -467,7 +467,7 @@ static int usb_stor_bulk_transfer_sglist(struct us_data *us, unsigned int pipe,
 int usb_stor_bulk_srb(struct us_data* us, unsigned int pipe,
 		      struct scsi_cmnd* srb)
 {
-	unsigned int partial;
+	unsigned int partial = 0;
 	int result = usb_stor_bulk_transfer_sglist(us, pipe, scsi_sglist(srb),
 				      scsi_sg_count(srb), scsi_bufflen(srb),
 				      &partial);
@@ -489,8 +489,8 @@ EXPORT_SYMBOL_GPL(usb_stor_bulk_srb);
 int usb_stor_bulk_transfer_sg(struct us_data* us, unsigned int pipe,
 		void *buf, unsigned int length_left, int use_sg, int *residual)
 {
-	int result;
-	unsigned int partial;
+	int result = 0;
+	unsigned int partial = 0;
 
 	/* are we scatter-gathering? */
 	if (use_sg) {
