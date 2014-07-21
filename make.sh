4aa6203a8ab689b044b58434bdec7eab7c0f5e87
Josue Rivera <prbassplayer@gmail.com>
2014-07-20 23:27:35 -0400

    Update bash script
      * Update script to know if I'm compiling locally or on server
    
    Change-Id: I4c996944622bf5c36644fa9c99a5fc10ed2733cd
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- make.sh
+++ make.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # Stupid shell script to compile kernel, nothing fancy
+ID="`pwd`"
 
 # Exports all the needed things Arch, SubArch and Cross Compile
 export ARCH=arm
 echo 'exporting Arch'
 export SUBARCH=arm
 echo 'exporting SubArch'
-#export CROSS_COMPILE=/home/prbassplayer/Honami/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.7/bin/arm-linux-androideabi-
-#export CROSS_COMPILE=/home/prbassplayer/WIP4.4/prebuilt/linux-x86/toolchain/linaro-4.7-13.04/bin/arm-linux-gnueabihf-
-export CROSS_COMPILE=/home/prbassplayer/caf/prebuilt/linux-x86/toolchain/linaro-4.8-13.06/bin/arm-linux-gnueabihf-
-echo 'exporting Cross Compile'
+if echo "$ID" | grep josh ; then
+export CROSS_COMPILE=/home/josh/Android/android_prebuilt/linux-x86/toolchain/linaro-4.9-14.06/bin/arm-linux-gnueabihf-
+echo 'exporting Cross Compile for HOME'
+else
+export CROSS_COMPILE=/home/prbassplayer/caf/prebuilt/linux-x86/toolchain/linaro-4.9-14.06/bin/arm-linux-gnueabihf-
+echo 'exporting Cross Compile server'
+fi
 
 # Make sure build is clean!
 echo 'Cleaning build'
